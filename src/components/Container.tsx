090a8cbb5211a3a5ea43b1034156741a76b81a13
Kevin Anthony <kevin.s.anthony@gmail.com>
2022-06-06 10:49:41 -0400

    edit box label (#10)
    * edit box label
    
    * cleanup log functions

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,258 +1,276 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faTrash, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { Typography } from "@mui/material";
-import { ContainerObj } from "../model/container";
-import { BackgroundGrid } from "./BackgroundGrid";
-import { ColorObj } from "../model/color";
-import { BinObj } from "../model/bin";
-import { BinNet } from "../net/bin";
-import { Bin } from "./Bin";
-import { BinEdit } from "./modal/BinEdit";
-import { ContentObj } from "../model/content";
-import { BoltObj } from "../model/bolt";
-import { hex2rgb } from "../util/formatting";
-import { red } from "@mui/material/colors";
-import { isEmpty } from "../util/utils";
-import { SearchBox } from "./modal/Search";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCog, faPlus, faSearch, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {Typography} from "@mui/material";
+import {ContainerObj} from "../model/container";
+import {BackgroundGrid} from "./BackgroundGrid";
+import {ColorObj} from "../model/color";
+import {BinObj} from "../model/bin";
+import {BinNet} from "../net/bin";
+import {Bin} from "./Bin";
+import {BinEdit} from "./modal/BinEdit";
+import {ContentObj} from "../model/content";
+import {BoltObj} from "../model/bolt";
+import {hex2rgb} from "../util/formatting";
+import {red} from "@mui/material/colors";
+import {isEmpty} from "../util/utils";
+import {SearchBox} from "./modal/Search";
+import {ContainerEdit} from "./modal/ContainerEdit";
 
 interface props {
-  container: ContainerObj;
-  setContainerByBinCallback: (bin: BinObj) => void;
-  binToHighlightID: string;
+    container: ContainerObj;
+    setContainerByBinCallback: (bin: BinObj) => void;
+    binToHighlightID: string;
 }
 
 export function Container({
-  container,
-  setContainerByBinCallback,
-  binToHighlightID,
-}: props) {
-  const [bins, setBins] = React.useState(() => container.bin);
-  const binNet = new BinNet();
+                              container,
+                              setContainerByBinCallback,
+                              binToHighlightID,
+                          }: props) {
+    const [bins, setBins] = React.useState(() => container.bin);
+    const binNet = new BinNet();
 
-  React.useEffect(() => {
-    setBins(container.bin);
-  }, [container]);
+    React.useEffect(() => {
+        setBins(container.bin);
+    }, [container]);
 
-  function removeBin(index: number) {
-    container.bin.splice(index, 1);
+    function removeBin(index: number) {
+        container.bin.splice(index, 1);
 
-    setBins([...container.bin]);
-  }
-
-  function drawNewBin(bin: BinObj) {
-    setNewBin(bin);
-    setNewBinIndex(newBinIndex);
-    setRedrawBin(true);
-
-    return;
-  }
-
-  function closeSearch() {
-    setPopupObject(<div />);
-  }
-
-  function saveBin(index: number, bin: BinObj, save: boolean) {
-    if (!save) {
-      container.bin.splice(index, 1, bin);
-      setBins([...container.bin]);
-
-      return;
-    }
-
-    if (isEmpty(bin.id)) {
-      bin.containerID = container.id;
-
-      binNet.createBin(bin).then((b: BinObj) => {
-        container.bin.push(b);
         setBins([...container.bin]);
-      });
-    } else {
-      binNet.putBin(bin).then((b: BinObj) => {
-        container.bin.splice(index, 1, b);
-        setBins([...container.bin]);
-      });
     }
-  }
 
-  const [newBinIndex, setNewBinIndex] = React.useState<number>(-1);
-  const [newBin, setNewBin] = React.useState<BinObj>(createNewBin());
-  const [redrawBin, setRedrawBin] = React.useState<boolean>(false);
-  const [newPopupObject, setPopupObject] = React.useState<JSX.Element>(<div />);
+    function drawNewBin(bin: BinObj) {
+        setNewBin(bin);
+        setNewBinIndex(newBinIndex);
+        setRedrawBin(true);
 
-  React.useEffect(() => {
-    setRedrawBin(false);
-
-    if (newBinIndex < 0) {
-      setPopupObject(<div />);
-    } else {
-      setPopupObject(
-        <Bin
-          highlight={false}
-          bin={newBin}
-          index={newBinIndex}
-          removeCallback={removeBin}
-          updateCallback={saveBin}
-        />
-      );
+        return;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newBinIndex, newBin, redrawBin]);
 
-  function createNewBin(): BinObj {
-    const newContent = ContentObj.Empty();
-    newContent.contentType = "bolt";
-    newContent.bolt = BoltObj.Empty();
+    function closePopup() {
+        setPopupObject(<div/>);
+    }
 
-    const newBin = BinObj.Empty();
-    newBin.content.push(newContent);
-    newBin.width = 5;
-    newBin.height = 5;
-    newBin.x = 0;
-    newBin.y = 0;
-    newBin.color = hex2rgb(red[500]);
+    function saveBin(index: number, bin: BinObj, save: boolean) {
+        if (!save) {
+            container.bin.splice(index, 1, bin);
+            setBins([...container.bin]);
 
-    return newBin;
-  }
+            return;
+        }
 
-  function handleNewBinClosed() {
-    setNewBinIndex(-1);
-    setNewBin(createNewBin());
-  }
+        if (isEmpty(bin.id)) {
+            bin.containerID = container.id;
 
-  function handleNewBinOpen() {
-    setNewBinIndex(container.bin.length + 1);
-  }
+            binNet.createBin(bin).then((b: BinObj) => {
+                container.bin.push(b);
+                setBins([...container.bin]);
+            });
+        } else {
+            binNet.putBin(bin).then((b: BinObj) => {
+                container.bin.splice(index, 1, b);
+                setBins([...container.bin]);
+            });
+        }
+    }
 
-  function handleSearchOpen() {
-    setPopupObject(
-      <SearchBox
-        closedCallback={closeSearch}
-        foundCallback={setContainerByBinCallback}
-      />
-    );
-  }
+    const [newBinIndex, setNewBinIndex] = React.useState<number>(-1);
+    const [newBin, setNewBin] = React.useState<BinObj>(createNewBin());
+    const [redrawBin, setRedrawBin] = React.useState<boolean>(false);
+    const [newPopupObject, setPopupObject] = React.useState<JSX.Element>(<div/>);
 
-  let gridGrid = Array(container.width)
-    .fill(null)
-    .map((_, posX) =>
-      Array(container.height)
-        .fill(null)
-        .map((_, posY) => (
-          <BackgroundGrid
-            key={`grid-${posX}-${posY}`}
-            background={ColorObj.Parse({ a: 0 })}
-            x={posX}
-            y={posY}
-          />
-        ))
-    );
+    React.useEffect(() => {
+        setRedrawBin(false);
 
-  return (
-    <>
-      <div
-        style={{
-          background: `rgb(${container.color.r}, ${container.color.g}, ${container.color.b})`,
-        }}
-      >
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: `1fr auto 1fr`,
-            fontSize: "32pt",
-            fontFamily: "Helvetica, sans-serif",
-            color: "white",
-            textAlign: "center",
-            padding: "10px",
-          }}
-        >
-          <Typography
-            variant="h2"
-            component="h2"
-            style={{ gridColumnStart: "2" }}
-          >
-            {container.label}
-          </Typography>
-          <div
-            style={{
-              gridColumnStart: "3",
-              display: "flex",
-              alignSelf: "center",
-              justifyContent: "right",
-              flexFlow: "row-reverse",
-            }}
-          >
-            <ButtonGroup
-              variant="contained"
-              aria-label="outlined contained button group"
-            >
-              <Button
-                onClick={handleNewBinOpen}
-                style={{
-                  padding: "4px",
-                  width: "4em",
-                  height: "4em",
-                }}
-              >
-                <FontAwesomeIcon icon={faPlus} />
-              </Button>
-              <Button
-                onClick={handleSearchOpen}
-                style={{
-                  padding: "4px",
-                  width: "4em",
-                  height: "4em",
-                }}
-              >
-                <FontAwesomeIcon icon={faSearch} />
-              </Button>
-              <Button
-                style={{
-                  padding: "4px",
-                  width: "4em",
-                  height: "4em",
-                }}
-                onClick={() => {}}
-              >
-                <FontAwesomeIcon icon={faTrash} />
-              </Button>
-            </ButtonGroup>
-          </div>
-        </div>
-        <div
-          className={"grid-container"}
-          style={{
-            width: `${container.width}${container.unit}`,
-            height: `${container.height}${container.unit}`,
-            gridTemplateColumns: `repeat(${container.width}, 1${container.unit})`,
-            gridTemplateRows: `repeat(${container.height}, 1${container.unit})`,
-          }}
-        >
-          {gridGrid}
-          {bins.map((bin: BinObj, index: number) => (
-            <Bin
-              highlight={binToHighlightID === bin.id}
-              removeCallback={removeBin}
-              updateCallback={saveBin}
-              key={`bin-${bin.id}`}
-              index={index}
-              bin={bin}
+        if (newBinIndex < 0) {
+            setPopupObject(<div/>);
+        } else {
+            setPopupObject(
+                <Bin
+                    highlight={false}
+                    bin={newBin}
+                    index={newBinIndex}
+                    removeCallback={removeBin}
+                    updateCallback={saveBin}
+                />
+            );
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [newBinIndex, newBin, redrawBin]);
+
+    function createNewBin(): BinObj {
+        const newContent = ContentObj.Empty();
+        newContent.contentType = "bolt";
+        newContent.bolt = BoltObj.Empty();
+
+        const newBin = BinObj.Empty();
+        newBin.content.push(newContent);
+        newBin.width = 5;
+        newBin.height = 5;
+        newBin.x = 0;
+        newBin.y = 0;
+        newBin.color = hex2rgb(red[500]);
+
+        return newBin;
+    }
+
+    function handleNewBinClosed() {
+        setNewBinIndex(-1);
+        setNewBin(createNewBin());
+    }
+
+    function handleNewBinOpen() {
+        setNewBinIndex(container.bin.length + 1);
+    }
+
+    function handleEditContainerOpen() {
+        setPopupObject(
+            <ContainerEdit closedCallback={closePopup} container={container}/>
+        )
+    }
+
+    function handleSearchOpen() {
+        setPopupObject(
+            <SearchBox
+                closedCallback={closePopup}
+                foundCallback={setContainerByBinCallback}
             />
-          ))}
-          {newPopupObject}
-        </div>
-      </div>
-      <BinEdit
-        bin={newBin}
-        title={"New Bin"}
-        binIndex={newBinIndex}
-        closedCallback={handleNewBinClosed}
-        updateCallback={drawNewBin}
-        removeCallback={removeBin}
-        saveCallback={saveBin}
-      />
-    </>
-  );
+        );
+    }
+
+    let gridGrid = Array(container.width)
+        .fill(null)
+        .map((_, posX) =>
+            Array(container.height)
+                .fill(null)
+                .map((_, posY) => (
+                    <BackgroundGrid
+                        key={`grid-${posX}-${posY}`}
+                        background={ColorObj.Parse({a: 0})}
+                        x={posX}
+                        y={posY}
+                    />
+                ))
+        );
+
+    return (
+        <>
+            <div
+                style={{
+                    background: `rgb(${container.color.r}, ${container.color.g}, ${container.color.b})`,
+                }}
+            >
+                <div
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns: `1fr auto 1fr`,
+                        fontSize: "32pt",
+                        fontFamily: "Helvetica, sans-serif",
+                        color: "white",
+                        textAlign: "center",
+                        padding: "10px",
+                    }}
+                >
+                    <Typography
+                        variant="h2"
+                        component="h2"
+                        style={{gridColumnStart: "2"}}
+                    >
+                        {container.label}
+                    </Typography>
+                    <div
+                        style={{
+                            gridColumnStart: "3",
+                            display: "flex",
+                            alignSelf: "center",
+                            justifyContent: "right",
+                            flexFlow: "row-reverse",
+                        }}
+                    >
+                        <ButtonGroup
+                            variant="contained"
+                            aria-label="outlined contained button group"
+                        >
+                            <Button
+                                onClick={handleNewBinOpen}
+                                style={{
+                                    padding: "4px",
+                                    width: "4em",
+                                    height: "4em",
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faPlus}/>
+                            </Button>
+                            <Button
+                                onClick={handleEditContainerOpen}
+                                style={{
+                                    padding: "4px",
+                                    width: "4em",
+                                    height: "4em",
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faCog}/>
+                            </Button>
+                            <Button
+                                onClick={handleSearchOpen}
+                                style={{
+                                    padding: "4px",
+                                    width: "4em",
+                                    height: "4em",
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faSearch}/>
+                            </Button>
+                            <Button
+                                style={{
+                                    padding: "4px",
+                                    width: "4em",
+                                    height: "4em",
+                                }}
+                                onClick={() => {
+                                }}
+                            >
+                                <FontAwesomeIcon icon={faTrash}/>
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+                </div>
+                <div
+                    className={"grid-container"}
+                    style={{
+                        width: `${container.width}${container.unit}`,
+                        height: `${container.height}${container.unit}`,
+                        gridTemplateColumns: `repeat(${container.width}, 1${container.unit})`,
+                        gridTemplateRows: `repeat(${container.height}, 1${container.unit})`,
+                    }}
+                >
+                    {gridGrid}
+                    {bins.map((bin: BinObj, index: number) => (
+                        <Bin
+                            highlight={binToHighlightID === bin.id}
+                            removeCallback={removeBin}
+                            updateCallback={saveBin}
+                            key={`bin-${bin.id}`}
+                            index={index}
+                            bin={bin}
+                        />
+                    ))}
+                    {newPopupObject}
+                </div>
+            </div>
+            <BinEdit
+                bin={newBin}
+                title={"New Bin"}
+                binIndex={newBinIndex}
+                closedCallback={handleNewBinClosed}
+                updateCallback={drawNewBin}
+                removeCallback={removeBin}
+                saveCallback={saveBin}
+            />
+        </>
+    );
 }
--- src/net/container.ts
+++ src/net/container.ts
@@ -1,24 +1,72 @@
-import { netCode } from "./net";
-import { ContainerObj } from "../model/container";
+import {netCode} from "./net";
+import {ContainerObj} from "../model/container";
 
 export class ContainerNet extends netCode {
-  getContainerAll(): Promise<Array<ContainerObj>> {
-    const url = this.getURL();
-    return window
-      .fetch(`${url}/container/all`, {
-        method: "GET",
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-      })
-      .then((r) => r.json())
-      .then<ContainerObj[]>((resp) => {
-        const out = new Array<ContainerObj>(0);
+    getContainerAll(): Promise<Array<ContainerObj>> {
+        const url = this.getURL();
+        return window
+            .fetch(`${url}/container/all`, {
+                method: "GET",
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+            })
+            .then((r) => r.json())
+            .then<ContainerObj[]>((resp) => {
+                const out = new Array<ContainerObj>(0);
 
-        for (let i in resp) {
-          out.push(ContainerObj.Parse(resp[i]));
-        }
+                for (let i in resp) {
+                    out.push(ContainerObj.Parse(resp[i]));
+                }
 
-        return out;
-      });
-  }
+                return out;
+            });
+    }
+
+    putContainer(container: ContainerObj): Promise<any> {
+        const json = JSON.stringify(container.JSON());
+
+        return window
+            .fetch(`${this.getURL()}/container/${container.id}`, {
+                method: "PUT",
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+                headers: {
+                    Host: window.location.origin,
+                    Origin: window.location.origin,
+                    "Content-Type": "application/json",
+                    "Content-Length": json.length.toString(),
+                },
+                body: json,
+            })
+            .then((r) => {
+                if (!r.ok) {
+                    throw r;
+                }
+
+                return r.json();
+            })
+            .then<ContainerObj>((data) => {
+                return ContainerObj.Parse(data);
+            });
+    }
+
+    deleteContainer(container: ContainerObj): Promise<ContainerObj> {
+        return window
+            .fetch(`${this.getURL()}/container/${container.id}`, {
+                method: "DELETE",
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+                headers: {
+                    Host: window.location.origin,
+                    Origin: window.location.origin,
+                },
+            })
+            .then((r) => {
+                if (!r.ok) {
+                    throw r;
+                }
+
+                return r.json();
+            });
+    }
 }
