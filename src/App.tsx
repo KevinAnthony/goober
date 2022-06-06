4352f35dbc5fd690a696685dc225e6a13b4d169e
Kevin Anthony <kevin.s.anthony@gmail.com>
2022-06-06 13:06:07 -0400

    new and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,77 @@
 import React from "react";
 import "./App.css";
 import "./containers.css";
-import { SideMenu } from "./components/SideMenu";
-import { ContainerNet } from "./net/container";
-import { ContainerObj } from "./model/container";
-import { Container } from "./components/Container";
-import { BinObj } from "./model/bin";
+import {SideMenu} from "./components/SideMenu";
+import {ContainerNet} from "./net/container";
+import {ContainerObj} from "./model/container";
+import {Container} from "./components/Container";
+import {BinObj} from "./model/bin";
 
 function App() {
-  const [containers, setContainerList] = React.useState<Array<ContainerObj>>(
-    []
-  );
-  const [highlightID, setHighlightID] = React.useState<string>("");
+    const [containers, setContainerList] = React.useState<Array<ContainerObj>>(
+        []
+    );
+    const [highlightID, setHighlightID] = React.useState<string>("");
 
-  const [container, setContainer] = React.useState<ContainerObj>(
-    ContainerObj.Empty
-  );
+    const [container, setContainer] = React.useState<ContainerObj>(
+        ContainerObj.Empty
+    );
+    const [newPopupObject, setPopupObject] = React.useState<JSX.Element>(<div/>);
 
-  React.useEffect(() => {
-    const netContainer = new ContainerNet();
+    React.useEffect(() => {
+        const netContainer = new ContainerNet();
 
-    netContainer.getContainerAll().then((containerList) => {
-      setContainerList(containerList);
-      if (containerList.length > 0) {
-        setContainer(containerList[0]);
-      }
-    });
-  }, []);
+        netContainer.getContainerAll().then((containerList) => {
+            setContainerList(containerList);
+            if (containerList.length > 0) {
+                setContainer(containerList[0]);
+            }
+        });
+    }, []);
 
-  function setContainerFromBin(bin: BinObj) {
-    setHighlightID(bin.id);
-    const found = containers.find((c) => c.id === bin.containerID);
-    setContainer(found ?? container);
-  }
+    function setContainerFromBin(bin: BinObj) {
+        setHighlightID(bin.id);
+        const found = containers.find((c) => c.id === bin.containerID);
+        setContainer(found ?? container);
+    }
 
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: `auto 1fr`,
-      }}
-    >
-      <div>
-        <SideMenu containers={containers} setOptionCallback={setContainer} />
-      </div>
-      <div
-        style={{
-          gridColumnStart: 2,
-          width: `${container.width}${container.unit}`,
-          fontSize: "32pt",
-          fontFamily: "FontAwesome, sans-serif",
-          color: "white",
-          padding: "10px",
-        }}
-      >
+    return (
         <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-          }}
+            style={{
+                display: "grid",
+                gridTemplateColumns: `auto 1fr`,
+            }}
         >
-          <Container
-            container={container}
-            binToHighlightID={highlightID}
-            setContainerByBinCallback={setContainerFromBin}
-          />
+            <div>
+                <SideMenu containers={containers} setOptionCallback={setContainer} setPopup={setPopupObject}/>
+                {newPopupObject}
+            </div>
+            <div
+                style={{
+                    gridColumnStart: 2,
+                    width: `${container.width}${container.unit}`,
+                    fontSize: "32pt",
+                    fontFamily: "FontAwesome, sans-serif",
+                    color: "white",
+                    padding: "10px",
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Container
+                        setPopup={setPopupObject}
+                        container={container}
+                        binToHighlightID={highlightID}
+                        setContainerByBinCallback={setContainerFromBin}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/modal/ContainerNew.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {Button, ButtonGroup, Dialog, DialogTitle, TextField,} from "@mui/material";
+
+import {ContainerObj} from "../../model/container";
+import {ContainerNet} from "../../net/container";
+
+interface props {
+    closedCallback: (container: ContainerObj) => void;
+}
+
+export function ContainerNew({closedCallback}: props) {
+    const net = new ContainerNet();
+    let container = ContainerObj.Empty();
+    container.unit = "cm"
+    container.height = 25
+    container.width = 33
+    container.color.r = 6
+    container.color.g = 74
+    container.color.b = 108
+    container.color.a = 255
+
+    return (
+        <div
+            style={{
+                position: "absolute",
+                backgroundColor: "rgb(77,77,77,1)",
+            }}
+        >
+            <Dialog
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={true}
+                onClose={closedCallback}
+            >
+                <DialogTitle>Edit Container</DialogTitle>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <TextField
+                        id="container-name"
+                        label="Name" variant="outlined"
+                        defaultValue={container.label}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            container.label = event.target.value;
+                        }}
+                    />
+                    <ButtonGroup
+                        aria-label="contained button group"
+                        variant="contained"
+                        style={{
+                            margin: "1em",
+                        }}
+                    >
+                        <Button
+                            style={{
+                                width: "16em",
+                                height: "4em",
+                            }}
+                            onClick={() => {
+                                net.createContainer(container).then((container: ContainerObj)=>{
+                                    closedCallback(container);
+                                })
+                            }}
+                        >
+                            Create
+                        </Button>
+                        <Button
+                            style={{
+                                width: "16em",
+                                height: "4em",
+                            }}
+                            onClick={() => {
+                                closedCallback(ContainerObj.Empty());
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                    </ButtonGroup>
+                </div>
+            </Dialog>
+        </div>
+    );
+}
