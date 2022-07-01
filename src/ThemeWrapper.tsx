935c50285b54ab97992c7403d16d9b96c3c8e45c
Kevin Anthony <kevin.s.anthony@gmail.com>
2022-07-01 10:22:35 -0400

    fix textbox number typing

--- src/ThemeWrapper.tsx
+++ src/ThemeWrapper.tsx
@@ -1,35 +1,31 @@
-import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { TypographyOptions } from "@mui/material/styles/createTypography";
 
 declare module "@mui/material/Typography" {
-    interface TypographyPropsVariantOverrides {
-        bin_content: true;
-    }
+  interface TypographyPropsVariantOverrides {
+    bin_content: true;
+  }
 }
 
 interface ExtendedTypographyOptions extends TypographyOptions {
-    bin_content: React.CSSProperties;
+  bin_content: React.CSSProperties;
 }
 const gooberTheme = createTheme({
-    typography: {
-        allVariants: {
-            color:"whitesmoke"
-        },
-        bin_content: {
-            whiteSpace:"pre",
-        }
-    } as ExtendedTypographyOptions,
+  typography: {
+    allVariants: {
+      color: "black",
+    },
+    bin_content: {
+      whiteSpace: "pre",
+    },
+  } as ExtendedTypographyOptions,
 });
 
 interface themeWrapperProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export default function ThemeWrapper({ children }: themeWrapperProps) {
-    return (
-        <ThemeProvider theme={gooberTheme}>
-            {children}
-        </ThemeProvider>
-    );
+  return <ThemeProvider theme={gooberTheme}>{children}</ThemeProvider>;
 }
