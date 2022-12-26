cdab15139829e8e16da68a2c101e24bc2c1e941b
Kevin Anthony <kevin.s.anthony@gmail.com>
2022-12-26 09:40:01 -0500

    add toast to app not main window

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import "./App.css";
 import "./containers.css";
 import ThemeWrapper from "./ThemeWrapper";
 import MainWindow from "./MainWindow";
-
+import { Toaster } from "react-hot-toast";
 
 function App() {
-    return (
-        <ThemeWrapper>
-            <MainWindow/>
-        </ThemeWrapper>
-    )
+  return (
+    <ThemeWrapper>
+      <div>
+        <Toaster />
+      </div>
+      <MainWindow />
+    </ThemeWrapper>
+  );
 }
 
 export default App;
