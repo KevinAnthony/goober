f4d4ae2de92e6520ea568fe56595d4cd7caa48a0
Kevin Anthony <kevin.s.anthony@gmail.com>
2023-01-16 14:29:19 -0500

    themed bin

--- src/util/colors.ts
+++ src/util/colors.ts
@@ -10,3 +10,11 @@ export const binGrey = "#607d8b";
 export const containerBoschGreen = "#064A6C";
 export const containerLawsonBrown = "#33250C";
 export const containerPencoBlue = "#5E92AE";
+
+// ThemeColors
+export const FontColor = "#05D9E8";
+export const DarkColor = "#094167";
+export const DarkColorHighlight = "#0E649E";
+export const BackgroundColor = "#0E141B";
+export const HighlightPink = "#FF2A6D";
+export const HighlightYellow = "#F2F000";
